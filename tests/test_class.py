b45e25c5f99d8b9585d56fc5ea85aefaf766ed38
Ben Puls <mail@benpuls.ru>
2024-11-04 16:52:46 +0500

    tests: add auth exception test

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,15 +1,22 @@
+import datetime
+
 import pytest
 
-from moy_nalog.exceptions import RefreshTokenNotFoundError
+from moy_nalog.exceptions import RefreshTokenNotFoundError, NalogMethodError
 from moy_nalog.moy_nalog import MoyNalog
 
 pytest_plugins = ("pytest_asyncio",)
 
 
-@pytest.mark.asyncio
-async def test_auth():
+class TestClass:
     nalog = MoyNalog("hello", "world")
-    try:
-        await nalog._make_auth()
-    except Exception as ex:
-        assert isinstance(ex, RefreshTokenNotFoundError)
+    
+    @pytest.mark.asyncio
+    async def test_auth_exception(self):
+        try:
+            await self.nalog.add_income("name", 123, datetime.datetime.now())
+        except Exception as ex:
+            # test exception
+            assert isinstance(ex, NalogMethodError)
+            # test message text from lknpd.nalog.ru API
+            assert str(ex).lower() == "указанный вами инн некорректен"
