f44509499ed7515725e09afa8e640c24b7dfc346
Ben Puls <mail@benpuls.ru>
2024-11-05 13:15:56 +0500

    tests: fox for updated method

--- tests/test_date_convertor.py
+++ tests/test_date_convertor.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 from moy_nalog.methods import AddIncomeMethod
 
@@ -14,15 +14,25 @@ class TestDateToLocalISO:
         # check on ISO 6801
         # https://ru.wikipedia.org/wiki/ISO_8601
 
-        assert self.method(datetime(2024, 10, 3)) == "2024-10-03T00:00:00+00:00"
-        assert self.method(datetime(2022, 3, 10)) == "2022-03-10T00:00:00+00:00"
+        assert self.method(datetime(2024, 10, 3)) == "2024-10-03T23:59:59+05:00"
+        assert self.method(datetime(2022, 3, 10)) == "2022-03-10T23:59:59+05:00"
 
     def test_date_unique(self):
         for _ in range(100):
-            date = datetime.now().replace(microsecond=0).replace(tzinfo=timezone.utc)
+            date = datetime.now().replace(microsecond=0)
             string_date = self.method(date)
-            assert date == datetime.fromisoformat(string_date[:-6])
+            # Currently
+            # if method get datetime object, then
+            # datetime object formatting ->
+            # hours=23, minutes=59, seconds=59
+            # with timezone
+            assert date.replace(
+                hour=23,
+                minute=59,
+                second=59,
+                tzinfo=timezone(timedelta(seconds=18000)),
+            ) == datetime.fromisoformat(string_date)
         for _ in range(100):
-            date = datetime.now().replace(microsecond=0).replace()
+            date = datetime.now().replace(microsecond=0)
             string_date = self.method()
-            assert date == datetime.fromisoformat(string_date[:-6])
+            assert date.astimezone() == datetime.fromisoformat(string_date)
