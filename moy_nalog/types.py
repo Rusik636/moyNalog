883cdc1261d58dfe3c8e1a36ebbf3b168e533788
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2024-11-05 17:56:39 +0500

    add: User entity

--- moy_nalog/types.py
+++ moy_nalog/types.py
@@ -17,3 +17,26 @@ class Income:
     print_url: str
 
     data: dict | None = None
+
+
+@dataclass
+class User:
+    last_name: str | None
+    id: int | None
+    display_name: str | None
+    middle_name: str | None
+    email: str | None
+    phone: str | None
+    inn: str | None
+    snils: str | None
+    avatar_exists: bool | None
+    initial_registration_date: datetime | None
+    registration_date: datetime | None
+    first_receipt_register_time: datetime | None
+    first_receipt_cancel_time: datetime | None
+    hide_cancelled_receipt: bool | None
+    register_available: str | None
+    status: str | None
+    restricted_mode: bool | None
+    pfr_url: str | None
+    login: str | None
