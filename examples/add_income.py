3c4fc7aed1151fc50fbb994ec884e60d652db79b
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2024-12-11 02:00:20 +0500

    add: first example

---
+++ examples/add_income.py
@@ -0,0 +1,13 @@
+import asyncio
+from moy_nalog import MoyNalog
+
+nalog = MoyNalog("1234567890", "MyStrongPassword")
+
+
+async def main():
+    await nalog.add_income(
+        "Предоставление информационных услуг #970/2495", amount=1000, quantity=1
+    )
+
+
+asyncio.run(main())
