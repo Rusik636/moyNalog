1b47e2ec34f3da0c0c7852e57f78082789572f6a
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2024-12-11 01:37:23 +0500

    add: Credentials, Income etc

--- moy_nalog/types.py
+++ moy_nalog/types.py
@@ -1,12 +1,27 @@
+from typing import Union, Optional
 from dataclasses import dataclass
 from datetime import datetime
+
+from moy_nalog.constants import CancelType
 
 
 @dataclass
 class Token:
     value: str
     expire_in: datetime
-    refresh_value: str | None = None
+    refresh_value: Optional[str] = None
+
+
+@dataclass
+class Credentials:
+    username: str
+    password: str
+
+
+@dataclass
+class AuthDetails:
+    inn: str
+    token: Token
 
 
 @dataclass
@@ -16,27 +31,48 @@ class Income:
     json_url: str
     print_url: str
 
-    data: dict | None = None
+    data: Optional[dict] = None
+
+
+@dataclass
+class CancellationInfo:
+    operation_time: datetime
+    register_time: datetime
+    tax_period_id: int
+    comment: CancelType
+
+
+@dataclass
+class CanceledIncome:
+    id: str
+    name: str
+    operation_time: datetime
+    request_time: datetime
+    payment_type: str  # TODO: CASH etc
+    partner_code: Optional[str]  # TODO: hz
+    total_amount: Union[int, float]
+    cancellation_info: CancellationInfo
+    source_device_id: str
 
 
 @dataclass
 class User:
-    last_name: str | None
-    id: int | None
-    display_name: str | None
-    middle_name: str | None
-    email: str | None
-    phone: str | None
-    inn: str | None
-    snils: str | None
-    avatar_exists: bool | None
-    initial_registration_date: datetime | None
-    registration_date: datetime | None
-    first_receipt_register_time: datetime | None
-    first_receipt_cancel_time: datetime | None
-    hide_cancelled_receipt: bool | None
-    register_available: str | None
-    status: str | None
-    restricted_mode: bool | None
-    pfr_url: str | None
-    login: str | None
+    last_name: Optional[str]
+    id: Optional[int]
+    display_name: Optional[str]
+    middle_name: Optional[str]
+    email: Optional[str]
+    phone: Optional[str]
+    inn: Optional[str]
+    snils: Optional[str]
+    avatar_exists: Optional[bool]
+    initial_registration_date: Optional[datetime]
+    registration_date: Optional[datetime]
+    first_receipt_register_time: Optional[datetime]
+    first_receipt_cancel_time: Optional[datetime]
+    hide_cancelled_receipt: Optional[bool]
+    register_available: Optional[str]
+    status: Optional[str]
+    restricted_mode: Optional[bool]
+    pfr_url: Optional[str]
+    login: Optional[str]
