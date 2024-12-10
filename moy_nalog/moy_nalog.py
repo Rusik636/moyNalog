2e3992e0d2ca1b8f0faa6f585433e67987307acf
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2024-12-11 01:37:14 +0500

    add: new methods

--- moy_nalog/moy_nalog.py
+++ moy_nalog/moy_nalog.py
@@ -1,114 +1,82 @@
-from typing import TypeVar, Optional, Union
-from datetime import datetime
-import random
-import string
-import time
+from __future__ import annotations
 
-from moy_nalog.methods import BaseMethod, AuthMethod, AddIncomeMethod, UserInfoMethod
-from moy_nalog.http import HttpConnection
-from moy_nalog.types import Token
+from typing import Any, Optional, Union
+from datetime import datetime, date
 
-T = TypeVar("T")
+from moy_nalog.methods import (
+    AddIncomeMethod,
+    CancelIncomeMethod,
+    UserMethod,
+)
+from moy_nalog.methods.api import BaseAPI
+from moy_nalog.http import HttpConnection, AuthDetails
+from moy_nalog.constants import CancelType
+from moy_nalog.types import Credentials, Income, CanceledIncome, User
 
 
 class MoyNalog:
     def __init__(self, login: str, password: str) -> None:
-        self._login: str = login
-        self.__password: str = password
-
-        self._device_id: str = self._create_device_id()
+        self.__login = login
+        self.__password = password
 
         self._connection: HttpConnection = self._init_http()
+        self._api: BaseAPI = self._init_api()
 
-        self._auth: AuthMethod = self._init_auth_method()
-        self._income: AddIncomeMethod = self._init_income_method()
-        self._user_info: UserInfoMethod = self._init_user_info_method()
-        self._authorized: bool = False
+    @property
+    def credentials(self) -> Credentials:
+        return Credentials(self.__login, self.__password)
 
-        self.__token: Optional[Token] = None
-        self.__inn: Optional[str] = None
+    @property
+    def auth_details(self) -> Optional[AuthDetails]:
+        return self._api.connection.auth.details
 
     def _init_http(self) -> HttpConnection:
-        return HttpConnection()
+        return HttpConnection(self.credentials)
 
-    def _init_auth_method(self) -> AuthMethod:
-        return AuthMethod(
-            self._connection, self._login, self.__password, self._device_id
-        )
-
-    def _init_income_method(self) -> AddIncomeMethod:
-        return AddIncomeMethod(self._connection)
-
-    def _init_user_info_method(self) -> UserInfoMethod:
-        return UserInfoMethod(self._connection)
-
-    def _create_device_id(self) -> str:
-        return "".join(random.choices(string.ascii_lowercase + string.digits, k=22))
-
-    async def _make_auth(self) -> dict:
-        inn, token = await self._auth.execute()
-        self.__inn = inn
-        self.__token = token
-
-        self._authorized = True
-
-    async def _get_token(self) -> str:
-        if (
-            self.__token
-            and self.__token
-            and int(time.time() * 1000) + 60 * 1000
-            < int(
-                datetime.fromisoformat(
-                    self.__token.expire_in.replace("Z", "+00:00")
-                ).timestamp()
-                * 1000
-            )
-        ):
-            return self.__token.value
-
-        if not self._authorized:
-            await self._make_auth()
-
-        self.__token = await self._auth.get_token_by_refresh_token(
-            connection=self._connection, refresh_token=self.__token.refresh_value
-        )
-
-        return self.__token.value
-
-    async def _call(self) -> dict:
-        pass
+    def _init_api(self) -> BaseAPI:
+        return BaseAPI(self._connection)
 
     async def add_income(
         self,
         name: str,
+        created_at: Union[datetime, date],
+        quantity: int,
         amount: Union[int, float],
-        quantity: int = 1,
-        date: str | datetime = datetime.now(),
-    ):
-        await self._execute_method(
-            self._income,
-            inn=self.__inn,
+    ) -> Income:
+        return await AddIncomeMethod(
+            api=self._api,
             name=name,
-            amount=amount,
+            created_at=created_at,
             quantity=quantity,
-            date=date,
-        )
+            amount=amount,
+        ).execute()
 
-    async def get_user_info(self):
-        return await self._execute_method(self._user_info)
+    async def cancel_income(
+        self, receipt_uuid: str, comment_type: CancelType
+    ) -> CanceledIncome:
+        return await CancelIncomeMethod(
+            api=self._api, comment_type=comment_type, receipt_uuid=receipt_uuid
+        ).execute()
 
-    async def _execute(self, method: T, **kwargs) -> T:
-        return await method.execute(**kwargs)
+    async def get_user_info(self) -> User:
+        return await UserMethod(api=self._api).execute()
 
-    async def _execute_method(self, method: BaseMethod, **kwargs) -> BaseMethod:
-        token = await self._get_token()
-        return await self._execute(method, token=token, **kwargs)
-
-    async def __aenter__(self):
+    async def __aenter__(self) -> MoyNalog:
         return self
 
-    async def __aexit__(self, exc_type, exc_value, traceback):
-        pass
+    async def __aexit__(self, exc_type, exc_value, traceback) -> None:
+        await self._connection.close()
+
+    async def close(self) -> None:
+        await self._connection.close()
 
     def __repr__(self) -> str:
         return "MoyNalog()"
+
+    def __hash__(self) -> int:
+        return hash(f"{self.credentials.username}:{self.credentials.password}")
+
+    def __eq__(self, value: Any) -> bool:
+        if not isinstance(value, MoyNalog):
+            return False
+        return hash(self) == hash(value)
